68f1218c60748d6283f79681a82e937bfd223f44
AnyaNecoder <ap.nerpina@gmail.com>
2024-09-24 08:54:26 +0600

    add inputmask

--- js/main.js
+++ js/main.js
@@ -36,3 +36,7 @@ closeBtn.addEventListener('click', () => {
     modal.close();
 });
 
+
+const inputs = document.querySelectorAll('input[type="tel"]');
+const im = new Inputmask('+7 (999) 999-99-99');
+im.mask(inputs);
